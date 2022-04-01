896eb42a2da72702e771de1b94d8a1f5c5d15436
sanodzegg <sanodzegg@gmail.com>
2022-04-01 18:28:02 +0400

    added functional

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Rate from './Rate.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Rate />
   </React.StrictMode>,
   document.getElementById('root')
 );
